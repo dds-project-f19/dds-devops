e510283e31fb591d1b0b3513247b710b201fc7ce
Your Name <you@example.com>
2019-12-01 01:34:52 +0000

    typo fixed

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-docker-compose up -d
-docker run -d -p "9000:9000" --network host back:latest
-# docker run -d -p "80:8080" front:latest
+docker build -t front:latest front/ --build-arg back_url=http://ec2-3-84-42-225.compute-1.amazonaws.com:9000 --no-cache && \
+	docker build -t back:latest back/ --no-cache && \
+	docker build -t dds-server:latest . --no-cache && \
+	docker-compose up -d
