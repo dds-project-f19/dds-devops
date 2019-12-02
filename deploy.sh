6a4dd7483c941627015180fe866ae445d942d485
Your Name <you@example.com>
2019-12-02 20:15:01 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t front:latest front/ --build-arg back_url=http://ec2-3-84-42-225.compute-1.amazonaws.com:9000 --no-cache && \
+docker build -t front:latest front/ --no-cache && \
 	docker build -t back:latest back/ --no-cache && \
 	docker build -t dds-server:latest . --no-cache && \
 	docker-compose up -d
--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-apt-get update
+
 apt-get install docker.io
 apt-get install git
 
